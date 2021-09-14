65465613613d7c0b1b4d57db2d476c923ea2d306
DeveloperDilantha <74175124+DeveloperDilantha@users.noreply.github.com>
2021-09-14 11:15:14 +0700

    Add files via upload

---
+++ rnd-font-awesome-free-5/rnd-font-awesome-free-5.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * RND Font Awesome Free
+ *
+ * @package           RNDCorePlugins
+ * @author            Developer Dilantha
+ * @copyright         2021 RND Innovations
+ * @license           GPL-2.0-or-later
+ *
+ * @rnd-plugin
+ * Plugin Name:       RND Font Awesome Free
+ * Plugin URI:        https://rnd.rodee.ca/core-plugins
+ * Description:       This plugin will add font awesome free icons to the theme. Read fontawesome license before using.
+ * Version:           5.15.3
+ * Requires at least: 1.0.0
+ * Requires PHP:      5.2
+ * Author:            Dilantha
+ * Author URI:        https://www.dilantha.org
+ * Text Domain:       dilantha
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+function rnd_font_awesome_free_add_all_to_head($items) {
+
+    $path   = APP_TMPL_URL . '/plugins/' . basename( dirname( __FILE__ ) ) ;
+    
+    $str = '<link rel="stylesheet" href="'.$path.'/css/all.css">'; 
+
+
+	return $items.$str;
+}
+add_filter('page_head', 'rnd_font_awesome_free_add_all_to_head');
