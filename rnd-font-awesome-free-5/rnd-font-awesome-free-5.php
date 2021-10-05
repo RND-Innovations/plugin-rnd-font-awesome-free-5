a88ccde663ec9bb42ea5f8e957b254e1a70208c7
DeveloperDilantha <74175124+DeveloperDilantha@users.noreply.github.com>
2021-10-05 23:26:15 +0700

    Updated Version
    Using an SEO friendly loading on pages

--- rnd-font-awesome-free-5/rnd-font-awesome-free-5.php
+++ rnd-font-awesome-free-5/rnd-font-awesome-free-5.php
@@ -12,7 +12,7 @@
  * Plugin Name:       RND Font Awesome Free
  * Plugin URI:        https://rnd.rodee.ca/core-plugins
  * Description:       This plugin will add font awesome free icons to the theme. Read fontawesome license before using.
- * Version:           5.15.3
+ * Version:           5.16.30
  * Requires at least: 1.0.0
  * Requires PHP:      5.2
  * Author:            Dilantha
@@ -26,9 +26,16 @@ function rnd_font_awesome_free_add_all_to_head($items) {
 
     $path   = APP_TMPL_URL . '/plugins/' . basename( dirname( __FILE__ ) ) ;
     
-    $str = '<link rel="stylesheet" href="'.$path.'/css/all.css">'; 
+    $str = ''; 
 
+    // JavaScript Active? load asynchronously
+    $str = '<link rel="preload" href="'.$path.'/css/all.css" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">';    
+    
+    // If No JavaScripts
+    $str .= '<noscript><link rel="stylesheet" href="'.$path.'/css/all.css"></noscript>';    
 
 	return $items.$str;
 }
 add_filter('page_head', 'rnd_font_awesome_free_add_all_to_head');
+
+
