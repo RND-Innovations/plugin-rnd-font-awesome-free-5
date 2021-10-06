9f491ce6eea82b695b252af2ba1ba95e35a8cec3
DeveloperDilantha <74175124+DeveloperDilantha@users.noreply.github.com>
2021-10-06 15:20:17 +0700

    Version 5.17.0
    Loading CSS after page load to speed the site up.

--- rnd-font-awesome-free-5/rnd-font-awesome-free-5.php
+++ rnd-font-awesome-free-5/rnd-font-awesome-free-5.php
@@ -12,7 +12,7 @@
  * Plugin Name:       RND Font Awesome Free
  * Plugin URI:        https://rnd.rodee.ca/core-plugins
  * Description:       This plugin will add font awesome free icons to the theme. Read fontawesome license before using.
- * Version:           5.16.30
+ * Version:           5.17.0
  * Requires at least: 1.0.0
  * Requires PHP:      5.2
  * Author:            Dilantha
@@ -28,14 +28,32 @@ function rnd_font_awesome_free_add_all_to_head($items) {
     
     $str = ''; 
 
-    // JavaScript Active? load asynchronously
-    $str = '<link rel="preload" href="'.$path.'/css/all.css" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">';    
-    
-    // If No JavaScripts
+    // If No JavaScripts load fonts on head
     $str .= '<noscript><link rel="stylesheet" href="'.$path.'/css/all.css"></noscript>';    
 
 	return $items.$str;
 }
 add_filter('page_head', 'rnd_font_awesome_free_add_all_to_head');
+
+// Load fonts without slowing page load
+function rnd_font_awesome_free_add_all_to_footer($items) {
+
+    $path   = APP_TMPL_URL . '/plugins/' . basename( dirname( __FILE__ ) ) ;
+    
+    $str = ''; 
+
+	$str .= '<script>';
+    // without jQuery (doesn't work in older IEs)
+	$str .= 'document.addEventListener("DOMContentLoaded", function(event) {';
+        $str .= 'var cssrndfa5 = document.createElement("link");';
+        $str .= 'cssrndfa5.rel = "stylesheet"; cssrndfa5.href="'.$path.'/css/all.css"; cssrndfa5.type="text/css";';
+        $str .= 'var defrndfa5 = document.getElementsByTagName("link")[1];';
+        $str .= 'defrndfa5.parentNode.insertBefore(cssrndfa5, defrndfa5);';
+	$str .= '});';    
+	$str .= '</script>';   
+
+	return $items.$str;
+}
+add_filter('page_footer', 'rnd_font_awesome_free_add_all_to_footer');
 
 
